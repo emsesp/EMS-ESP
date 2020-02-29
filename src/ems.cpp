a35c363d0839792ea98ad10ebfd9d7a1c014fa53
MichaelDvP <github@mdvp.de>
2020-02-29 09:30:18 +0100

    Sync with upstream/dev

--- src/ems.cpp
+++ src/ems.cpp
@@ -2321,25 +2321,25 @@ void ems_setThermostatTemp(float temperature, uint8_t hc_num, uint8_t temptype) 
     EMS_TxTelegram.action = EMS_TX_TELEGRAM_WRITE;
     EMS_TxTelegram.dest   = device_id;
 
-    if (model == EMS_DEVICE_FLAG_RC20) {
-        EMS_TxTelegram.type               = EMS_TYPE_RC20Set;
-        EMS_TxTelegram.offset             = EMS_OFFSET_RC20Set_temp;
-        EMS_TxTelegram.comparisonPostRead = EMS_TYPE_RC20StatusMessage;
-        EMS_TxTelegram.type_validate      = EMS_TxTelegram.type;
-
-    } else if (model == EMS_DEVICE_FLAG_RC10) {
+    if (model == EMS_DEVICE_FLAG_RC10) {
         EMS_TxTelegram.type               = EMS_TYPE_RC10Set;
         EMS_TxTelegram.offset             = EMS_OFFSET_RC10Set_temp;
         EMS_TxTelegram.comparisonPostRead = EMS_TYPE_RC10StatusMessage;
         EMS_TxTelegram.type_validate      = EMS_TxTelegram.type;
-
-    } else if (model == EMS_DEVICE_FLAG_RC30) {
+    }
+    else if (model == EMS_DEVICE_FLAG_RC20) {
+        EMS_TxTelegram.type               = EMS_TYPE_RC20Set;
+        EMS_TxTelegram.offset             = EMS_OFFSET_RC20Set_temp;
+        EMS_TxTelegram.comparisonPostRead = EMS_TYPE_RC20StatusMessage;
+        EMS_TxTelegram.type_validate      = EMS_TxTelegram.type;
+    }
+    else if (model == EMS_DEVICE_FLAG_RC30) {
         EMS_TxTelegram.type               = EMS_TYPE_RC30Set;
         EMS_TxTelegram.offset             = EMS_OFFSET_RC30Set_temp;
         EMS_TxTelegram.comparisonPostRead = EMS_TYPE_RC30StatusMessage;
         EMS_TxTelegram.type_validate      = EMS_TxTelegram.type;
-
-    } else if (model == EMS_DEVICE_FLAG_RC300) {
+    }
+    else if (model == EMS_DEVICE_FLAG_RC300) {
         // check mode to determine offset
         if (EMS_Thermostat.hc[hc_num - 1].mode == 1) {        // auto
             EMS_TxTelegram.offset = 0x08;                     // auto offset
@@ -2362,8 +2362,8 @@ void ems_setThermostatTemp(float temperature, uint8_t hc_num, uint8_t temptype) 
         }
 
         EMS_TxTelegram.type_validate = EMS_ID_NONE; // validate by reading from a different telegram
-
-    } else if ((model == EMS_DEVICE_FLAG_RC35) || (model == EMS_DEVICE_FLAG_RC30N)) {
+    }
+    else if ((model == EMS_DEVICE_FLAG_RC35) || (model == EMS_DEVICE_FLAG_RC30N)) {
         switch (temptype) {
         case 1: // change the night temp
             EMS_TxTelegram.offset = EMS_OFFSET_RC35Set_temp_night;
@@ -2414,7 +2414,6 @@ void ems_setThermostatTemp(float temperature, uint8_t hc_num, uint8_t temptype) 
         }
         EMS_TxTelegram.type_validate = EMS_TxTelegram.type;
     }
-
     else if (model == EMS_DEVICE_FLAG_JUNKERS) {
         switch (temptype) {
         case 1: // change the no frost temp
